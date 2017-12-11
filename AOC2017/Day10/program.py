34e8cc56fb70325a9b38dc4a78915610adcb0987
Evania Mans <em1419@cs.ship.edu>
2017-12-11 16:34:59 -0500

    First check-in for Day 10 (not finished yet...)

---
+++ AOC2017/Day10/program.py
@@ -0,0 +1,34 @@
+"""
+    Program for Advent of Code Day 10
+"""
+
+import numpy
+import itertools
+
+print("Day 10, Part 1")
+
+maxIndex = 256
+lengths = []
+
+testMaxIndex = 5
+testlengths = [3,4,1,5]
+
+mylist = [0,1,2,3,4]
+
+currentIndex = 0
+skipsize = 0
+
+for length in testlengths:
+    start = currentIndex
+    end = length + skipsize
+
+    mylist[start:end] = mylist[start:end][::-1]
+    print("mylist[%d:%d] = %s" % (start, end, mylist))
+    currentIndex = length + skipsize
+
+    if (length + skipsize >= len(testlengths)):
+        currentIndex = 
+
+    skipsize = skipsize + 1
+    print("currentindex (end) = %d") % (currentIndex)
+    print("skipsize (end) = %d") % (skipsize)
